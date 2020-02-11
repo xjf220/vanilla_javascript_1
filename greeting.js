eb6cf676c7913e5bd984fb4a17b264676f268b1e
xjf220 <60065957+xjf220@users.noreply.github.com>
2020-02-11 22:47:52 +0900

    #3.3 Saving the User Name part One

---
+++ greeting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // currentUser 값이 없는 경우
+  } else {
+    // currentUser 값이 있는 경우
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
