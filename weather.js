7e2aa2b495217a9f96b843da3dbdd32821024e65
xjf220 <60065957+xjf220@users.noreply.github.com>
2020-02-15 23:37:19 +0900

    #3.9 Getting the Weather part One (Geolocation)

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "dd9668eab0034fa8541e71c7456ecd3c";
+const COORDS = `coords`
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
